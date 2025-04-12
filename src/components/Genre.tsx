f54fcc1b3a84a4aa6515fedf033d0cda69322d64
Erick Riaño <123erickrh@gmail.com>
2025-04-12 11:47:33 -0500

    Add AlbumCard component and integrate into Genre component

--- src/components/Genre.tsx
+++ src/components/Genre.tsx
@@ -1,3 +1,5 @@
+import AlbumCard from './AlbumCard';
+
 export default function Genre() {
   return (
     <section>
@@ -6,10 +8,95 @@ export default function Genre() {
         alt=''
         className='h-50 w-screen object-cover object-center shadow-md'
       />
-      <div className='container m-5 mx-auto px-10'>
+      <article className='container m-5 mx-auto px-10'>
         <h1 className='text-2xl font-bold'>Albums</h1>
-        <p className='text-gray-600'>This is the Albums page.</p>
-      </div>
+        <p className='text-gray-600'>Enjoy every last drop of sound</p>
+
+        <div className='mt-10 flex flex-wrap gap-x-5 gap-y-15'>
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+          <AlbumCard
+            cover='https://www.metro951.com/wp-content/uploads/2015/05/Marilyn-Manson.jpg'
+            name='Marilyn Manson'
+          />
+        </div>
+      </article>
+
+      <div></div>
     </section>
   );
 }
