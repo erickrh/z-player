c7b34243028fedd0df888a4880dc4ac56a893c23
Erick Riaño <123erickrh@gmail.com>
2025-05-23 23:13:56 -0500

    Adjust spacing in GenresSection component for improved layout

--- src/components/GenresSection.tsx
+++ src/components/GenresSection.tsx
@@ -6,7 +6,7 @@ export default function GenresSection() {
     <>
       <section>
         <h2 className='font-karla mt-1 text-3xl font-normal'>Genres</h2>
-        <section className='mt-4 flex flex-wrap justify-center gap-5'>
+        <section className='mt-4 flex flex-wrap justify-center gap-3'>
           {genresData.map((genre) => (
             <GenreCard
               key={genre.id}
