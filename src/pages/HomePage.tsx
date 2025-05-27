5e86e5f1e77d6fad9fe1ecb7eb7c3b30f436cdb4
Erick Riaño <123erickrh@gmail.com>
2025-05-27 00:15:53 -0500

    Update HomePage component for responsive padding adjustments

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ import Header from '../components/Header';
 
 function HomePage() {
   return (
-    <div className='container mx-auto mb-5 px-10'>
+    <div className='container mx-auto mb-5 sm:px-10'>
       <Header />
       <GenresSection />
       <Footer />
