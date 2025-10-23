3c0f5d031cd62d19e523348cd68f94080acb2eca
Erick Riaño <123erickrh@gmail.com>
2025-10-22 20:15:13 -0500

    Improve genres section component

--- src/components/GenresSection.tsx
+++ src/components/GenresSection.tsx
@@ -3,20 +3,21 @@ import genresData from '@/data/genresData.json';
 
 export default function GenresSection() {
   return (
-    <>
-      <section>
-        <h2 className='font-karla mt-1 text-3xl font-normal'>Genres</h2>
-        <section className='mt-4 flex flex-wrap justify-center gap-3'>
-          {genresData.map((genre) => (
-            <GenreCard
-              key={genre.id}
-              name={genre.name}
-              cover={genre.cover}
-              id={genre.id}
-            />
-          ))}
-        </section>
-      </section>
-    </>
+    <section className='mt-4'>
+      <div className='mt-4 mr-5 flex flex-col items-end sm:-mt-2 sm:mr-44'>
+        <h1 className='font-karla text-2xl font-bold'>Genres</h1>
+        <p className='text-gray-600'>Welcome to z-player</p>
+      </div>
+      <div className='mt-6 flex flex-wrap justify-center gap-3'>
+        {genresData.map((genre) => (
+          <GenreCard
+            key={genre.id}
+            name={genre.name}
+            cover={genre.cover}
+            id={genre.id}
+          />
+        ))}
+      </div>
+    </section>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,8 @@ import Header from '../components/Header';
 
 function HomePage() {
   return (
-    <div className='container mx-auto mb-5 sm:px-10'>
+    // <div className='container mx-auto mb-5 sm:px-10'>
+    <div className='flex flex-col items-center'>
       <Header />
       <GenresSection />
       <Footer />
